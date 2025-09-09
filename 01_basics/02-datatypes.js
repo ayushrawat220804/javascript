31f9c46206eb83b0c76b72b884d2d08b3d2ee551
Ayush Rawat <ayushrawat220804@gmail.com>
2025-09-09 05:51:06 +0000

    datatypes in javascript

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,45 @@
+console.log("data types in javascript")
+
+
+"use strict"; // treat all js code as newer version
+
+/*
+alert("hello boss!, namoskaar"); // will give an error because we are in node right now.
+alert( 2 + 4 ) // this is also ok but will not work via node js , not browser
+
+
+mdn documentions or tc39.es ecmascript , java script standards 
+
+*/
+
+
+
+// console.log(2 + 4); console.log("ayush")
+// // code readability should be high
+// // or
+// console.log( 4 - 3)
+// console.log("karan")
+
+
+let name = "ayush rawat"
+let num = 123
+let isloggedin = true
+
+// number => 2 to power 53
+// bigint for very large numbers
+// string , can be used in single or double quotes
+// boolean , true or false || 0 , 1
+// null , it is also standalone value in js , it is also a type
+// undefined , means values is not assigned
+let state = null
+// symbol , to find uniqueness of a component
+
+// object 
+
+console.log(typeof name) // tells the type eg string, int etc
+console.log(typeof null) // will return object , means null is a object, lol language error 
+console.log(typeof undefined) // will return undefined
+
+
+
+
