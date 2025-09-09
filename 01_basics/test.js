4a42409308f14aa1930185a855f5b2261c9a6ae3
Ayush Rawat <ayushrawat220804@gmail.com>
2025-09-09 05:28:04 +0000

    const, var, let

---
+++ 01_basics/test.js
@@ -0,0 +1,21 @@
+const accountid = 20231;
+let accountemailid = "ayushrawat220804@gmail.com"
+var accountpassword = "Xmn@30_2233"
+accountcity = "dehradun"
+let accountstatevalue // what will be the output?
+
+// accountid = 2;
+accountemailid = "ayushrawat"
+accountpassword = "234"
+accountcity = "india"
+
+
+console.log(accountid)
+console.table([accountemailid,accountid,accountpassword,accountcity,accountstatevalue])
+
+/*
+
+prefer not to use var
+because of issues in block scope and functional scope 
+
+*/
